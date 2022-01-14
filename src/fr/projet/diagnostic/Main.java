200b62bf949637483c4efcac5eb7d54354e8a25e
Mael <dinocraftdidi@gmail.com>
2022-01-14 10:17:17 +0100

    edit main

--- src/fr/projet/diagnostic/Main.java
+++ src/fr/projet/diagnostic/Main.java
@@ -1,10 +1,30 @@
 package fr.projet.diagnostic;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import fr.projet.diagnostic.util.FileManager;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		analyse();
 
+	}
+	
+	public static void analyse() {
+		FileManager fm = new FileManager(new File("regle.txt"));
+		try {
+			List<String> lines = fm.readAllLine();
+			System.out.println(lines.get(0));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
